938c91131d51e6b5057cabf7f6404c18ac3d3b62
george <a7587463@gmail.com>
2020-11-11 23:51:51 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WP_2020_01_HW01_3A713105
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnrun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputttb = new System.Windows.Forms.TextBox();
+            this.outputtB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnrun
+            // 
+            this.btnrun.Location = new System.Drawing.Point(565, 72);
+            this.btnrun.Name = "btnrun";
+            this.btnrun.Size = new System.Drawing.Size(148, 52);
+            this.btnrun.TabIndex = 0;
+            this.btnrun.Text = "執行";
+            this.btnrun.UseVisualStyleBackColor = true;
+            this.btnrun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 25F);
+            this.label1.Location = new System.Drawing.Point(147, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "請輸入";
+            // 
+            // inputttb
+            // 
+            this.inputttb.Location = new System.Drawing.Point(153, 72);
+            this.inputttb.Name = "inputttb";
+            this.inputttb.Size = new System.Drawing.Size(223, 22);
+            this.inputttb.TabIndex = 1;
+            // 
+            // outputtB
+            // 
+            this.outputtB.Location = new System.Drawing.Point(153, 229);
+            this.outputtB.Multiline = true;
+            this.outputtB.Name = "outputtB";
+            this.outputtB.ReadOnly = true;
+            this.outputtB.Size = new System.Drawing.Size(255, 187);
+            this.outputtB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 25F);
+            this.label2.Location = new System.Drawing.Point(147, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 34);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "輸出";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.outputtB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputttb);
+            this.Controls.Add(this.btnrun);
+            this.Name = "Form1";
+            this.Text = "MD5轉換";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnrun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputttb;
+        private System.Windows.Forms.TextBox outputtB;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WP_2020_01_HW01_3A713105
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cryptoMD5 = System.Security.Cryptography.MD5.Create();
+            var bytes = Encoding.UTF8.GetBytes(inputttb.Text);
+            var hash = cryptoMD5.ComputeHash(bytes);
+            var md5 = BitConverter.ToString(hash);
+            outputtB.Text = md5;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
